3b990acf78eece865ffa85773a2878c393cd0ad0
cheng <732345907@qq.com>
2019-11-15 14:17:06 +0800

    add

---
+++ runtime/temp/da9ad1738d104714029dffc4039fcd0d.php
@@ -0,0 +1,13 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:108:"C:\Users\Administrator\Desktop\phpEnv5.6.0-Green\www\hzs\public/../application/home\view\searches\index.html";i:1573779806;}*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>惠优税</title>
+</head>
+<body>
+<div style="width:100%;text-align:center;">
+    <img src="/static/hzs_error.png" style="width:1280px;height:800px;">
+</div>
+</body>
+</html>
