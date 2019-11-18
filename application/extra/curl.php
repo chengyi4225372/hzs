700274c124abc0a353fbf6da4fa1c954ed8d6618
‘jason <syl_880921@163.com>
2019-11-18 10:29:57 +0800

    修改77

--- application/extra/curl.php
+++ application/extra/curl.php
@@ -3,4 +3,6 @@ return [
     'hlg' => 'http://172.26.3.13:8009/index.php',//惠灵工
     'website' => 'http://172.26.3.13:8008/index.php',//官网
     'hzs' => 'http://172.26.3.13:8010/index.php',//会找事
+    'ge_login_url' => 'http://172.26.3.12:8009/#/logingr',
+    'gr_register_url' => 'http://172.26.3.12:8009/#/registergr',
 ];
--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -15,12 +15,7 @@ class Index extends BaseController
 
     public function index()
     {
-//        Cookie::clear('mobile');
-//        Cookie::clear('token');
-//        Cookie::clear('userName');
-//        Cookie::clear('userType');
-//        var_dump(Cache::set(18681524382,'')).'<br>';
-//        var_dump(Cookie::get());exit;
+
         if ($this->request->isGet()) {
             //用户信息
             $this->assign('userinfo',$this->userinfo);
