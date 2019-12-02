2260ec75fcab47ad8d171efb854846177139a575
‘jason <syl_880921@163.com>
2019-12-02 11:49:14 +0800

    提交

--- application/extra/curl.php
+++ application/extra/curl.php
@@ -1,8 +1,16 @@
 <?php
 return [
     'hlg' => 'http://172.26.3.13:8009/index.php',//惠灵工
+//    'hlg' => 'http://www.i35tax.com',//惠灵工
+
     'website' => 'http://172.26.3.13:8008/index.php',//官网
+//    'website' => 'http://www.35service.com',//官网
+
     'hzs' => 'http://172.26.3.13:8010/index.php',//会找事
+//    'hzs' => 'http://www.hqyonline.com',//会找事
+
+    'hys' => 'http://172.26.3.13:8011/index.php',//惠优税
+
     'ge_login_url' => 'http://172.26.3.12:8009/#/logingr',
     'gr_register_url' => 'http://172.26.3.12:8009/#/registergr',
     'hou_tai_url' => 'http://172.26.3.12:8009/#',
